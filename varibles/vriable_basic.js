e83b2694b1c05938bc408716dcd38a72534c6019
Malaygajera04 <malaygajera23@gmail.com>
2025-02-13 08:59:14 +0530

    Add files via upload

---
+++ varibles/vriable_basic.js
@@ -0,0 +1,17 @@
+const accountid=13;
+let mailid="lku@33gmail.com"
+var accountname="saving"
+
+// accountid=13 not allowed
+
+
+mailid="djiodj"
+accountname="current"
+console.log(accountname);//allowed mail id value can be change
+
+/*
+var is can not prefer
+because this generate the issue in scooping block
+*/
+console.table([accountid,mailid,accountname])/*this is the given the
+ value of tabuler from*/
