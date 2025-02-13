c77861439d3b18cba8530c47b91643271ab1d6c0
Malaygajera04 <malaygajera23@gmail.com>
2025-02-13 09:37:23 +0530

    Add files via upload

---
+++ datatype/basic_datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //this is can be use to the the new version of java script
+//alert(5+6);  here can be use node js not a browser
+console.log(
+    3+3
+)//this type not write in code  because code readablity is high
+
+//datatype
+
+//Number=>2 to power 53 value
+//string1 ="jkl";
+//let state;=>undefined
+//bollean=>true or false
+//null=>object
+
+//typeof => this is can be given the type of data type
+ console.log(typeof "jkl")// jkl is type is string
+ console.log(typeof null);//type of null is object
+ console.log(typeof undefined);//type of undefind is undefind
+ 
+ 
+ 
